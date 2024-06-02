7995ed1cd09518ee3fcd080ac15a60f72fa0579e
cloedy <skywangqj@gmail.com>
2024-06-02 20:42:56 +0800

    fix: config

--- cmd/protogen.go
+++ cmd/protogen.go
@@ -1,15 +1,15 @@
 package cmd
 
 import (
-    "os"
-    "strings"
-    "text/template"
-    "unicode"
+	"os"
+	"strings"
+	"text/template"
+	"unicode"
 )
 
 type datam struct {
-    Name  string
-    Name2 string
+	Name  string
+	Name2 string
 }
 
 const prototpl = `
@@ -28,39 +28,39 @@ message {{.Name}}Request {
   string {{.Name2}}_name = 2 [json_name = "{{.Name2}}_name", (validate.rules).string = {ignore_empty: true, max_len: 255}];
 }
 
-message {{.Name}}Return {
+message {{.Name}}Response {
   int64 code = 1;
   string msg = 2;
 }
 `
 
 func protogen(name string) {
-    name2 := name
-    name = strings.ToUpper(name[0:1]) + name[1:]
-    name2 = camelToSnake(name)
-    data := datam{Name: name, Name2: name2}
+	name2 := name
+	name = strings.ToUpper(name[0:1]) + name[1:]
+	name2 = camelToSnake(name)
+	data := datam{Name: name, Name2: name2}
 
-    tmpl, err := template.New("p3").Parse(prototpl)
-    if err != nil {
-        panic(err)
-    }
-    if err := tmpl.Execute(os.Stdout, data); err != nil {
-        panic(err)
-    }
+	tmpl, err := template.New("p3").Parse(prototpl)
+	if err != nil {
+		panic(err)
+	}
+	if err := tmpl.Execute(os.Stdout, data); err != nil {
+		panic(err)
+	}
 
 }
 
 func camelToSnake(s string) string {
-    result := make([]rune, 0, len(s)*2)
-    for i, runeValue := range s {
-        if unicode.IsUpper(runeValue) {
-            if i > 0 {
-                result = append(result, '_')
-            }
-            result = append(result, unicode.ToLower(runeValue))
-        } else {
-            result = append(result, runeValue)
-        }
-    }
-    return string(result)
+	result := make([]rune, 0, len(s)*2)
+	for i, runeValue := range s {
+		if unicode.IsUpper(runeValue) {
+			if i > 0 {
+				result = append(result, '_')
+			}
+			result = append(result, unicode.ToLower(runeValue))
+		} else {
+			result = append(result, runeValue)
+		}
+	}
+	return string(result)
 }
